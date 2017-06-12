7de95d42b4bae172710b761224f5266733627985
nachoblancobon <nachobb33@gmail.com>
2017-06-11 23:24:40 -0300

    #21

--- src/main/java/model/repositories/Repositorios.java
+++ src/main/java/model/repositories/Repositorios.java
@@ -2,7 +2,5 @@ package model.repositories;
 
 public class Repositorios {
 	public static EmpresasRepository empresas = new EmpresasRepository();
-	public static CuentasRepository cuentas = new CuentasRepository();
-	public static IndicadoresRepository indicadores = new IndicadoresRepository();
 	public static TipoParametros parametros = new TipoParametros();
 }
