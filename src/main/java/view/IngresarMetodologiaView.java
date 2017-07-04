5a2f492abed0339f8d813b42b3bc0b47c1ac2777
nachoblancobon <nachobb33@gmail.com>
2017-07-04 19:24:30 -0300

    avanzo view de ingresar metodologias y ejecutar metodologias

--- src/main/java/view/IngresarMetodologiaView.java
+++ src/main/java/view/IngresarMetodologiaView.java
@@ -1,10 +1,14 @@
 package view;
 
+import org.uqbar.arena.widgets.Button;
 import org.uqbar.arena.widgets.Label;
 import org.uqbar.arena.widgets.Panel;
+import org.uqbar.arena.widgets.Selector;
 import org.uqbar.arena.windows.Dialog;
 import org.uqbar.arena.windows.WindowOwner;
 
+import model.Comparador;
+import model.Indicador;
 import viewmodel.IngresarMetodologiaViewModel;
 
 @SuppressWarnings("serial")
@@ -12,13 +16,31 @@ public class IngresarMetodologiaView extends Dialog<IngresarMetodologiaViewModel
 
 	public IngresarMetodologiaView(WindowOwner owner) {
 		super(owner, new IngresarMetodologiaViewModel());
+		this.getModelObject().obtenerIndicadores();
+		this.getModelObject().obtenerComparadores();
 	}
 
 	@Override
 	protected void createFormPanel(Panel mainPanel) {
 		this.setTitle("Ingreso de metodologias");
 		
-
+		new Label(mainPanel).setText("Ingresar Indicador de la metodologia");
+		
+		Selector<Indicador> selectorIndicadores = new Selector<Indicador>(mainPanel);
+		selectorIndicadores.bindItemsToProperty("indicadores").adaptWith(Indicador.class, "nombre");
+		selectorIndicadores.bindValueToProperty("indicadorSeleccionado");
+		
+		new Label(mainPanel).setText("Ingresar comparador");
+		
+		Selector<Comparador> selectorComparadores = new Selector<Comparador>(mainPanel);
+		selectorComparadores.bindItemsToProperty("comparadores").adaptWith(Comparador.class, "nombreComparador");
+		selectorComparadores.bindValueToProperty("comparadorSeleccionado");
+		
+		new Button(mainPanel).setCaption("Ingresar metodologia").onClick(this::ingresarMetodologia);
+		
 	}
-
+	public void ingresarMetodologia(){
+		this.getModelObject().ingresarMetodologia();
+		this.close();
+	}
 }
--- src/main/java/viewmodel/IngresarMetodologiaViewModel.java
+++ src/main/java/viewmodel/IngresarMetodologiaViewModel.java
@@ -1,5 +1,74 @@
 package viewmodel;
 
-public class IngresarMetodologiaViewModel {
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
+import org.uqbar.commons.utils.Observable;
+
+import Services.IndicadoresService;
+import Services.MetodologiasService;
+import model.Comparador;
+import model.ComparadorUnoMayorQueOtro;
+import model.Indicador;
+import model.Metodologia;
+import model.repositories.Repositorios;
+
+@Observable
+public class IngresarMetodologiaViewModel {
+	private List<Indicador> indicadores;
+	private Indicador indicadorSeleccionado;
+	private Comparador comparadorSeleccionado;
+	private Metodologia metodologia = new Metodologia(null);
+	private List<Comparador> comparadores;
+	
+	public void obtenerComparadores(){
+		comparadores = Repositorios.metodologias.allComparadores();
+	}
+	
+	public void ingresarMetodologia() {
+		metodologia.setComparador(comparadorSeleccionado);
+		try {
+			MetodologiasService.guardarMetodologiaEnServicioExterno(metodologia);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void obtenerIndicadores() {
+		try {
+			indicadores = IndicadoresService.obtenerInicadoresDeServicioExterno();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List<Indicador> getIndicadores() {
+		return indicadores;
+	}
+	public void setIndicadores(List<Indicador> indicadores) {
+		this.indicadores = indicadores;
+	}
+	public Indicador getIndicadorSeleccionado() {
+		return indicadorSeleccionado;
+	}
+	public void setIndicadorSeleccionado(Indicador indicadorSeleccionado) {
+		this.indicadorSeleccionado = indicadorSeleccionado;
+	}
+	public Comparador getComparadorSeleccionado() {
+		return comparadorSeleccionado;
+	}
+	public void setComparadorSeleccionado(Comparador comparadorSeleccionado) {
+		this.comparadorSeleccionado = comparadorSeleccionado;
+	}
+
+	public List<Comparador> getComparadores() {
+		return comparadores;
+	}
+
+	public void setComparadores(List<Comparador> comparadores) {
+		this.comparadores = comparadores;
+	}
+	
 }
