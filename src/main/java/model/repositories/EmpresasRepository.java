dc29c6577f8b88c38bb8e59cddef1325b526e5b8
nachoblancobon <nachobb33@gmail.com>
2017-09-10 15:46:33 -0300

    relacion cuenta-empresa

--- src/main/java/model/repositories/EmpresasRepository.java
+++ src/main/java/model/repositories/EmpresasRepository.java
@@ -4,12 +4,17 @@ import java.io.FileNotFoundException;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import Services.EmpresasService;
 import model.Empresa;
 
 public class EmpresasRepository {
 	private List<Empresa> empresas = new LinkedList<>();
 
+	@SuppressWarnings("unchecked")
 	public List<Empresa> all() {
 		try {
 			return EmpresasService.obtenerEmpresasDeServicioExterno();
@@ -17,5 +22,8 @@ public class EmpresasRepository {
 			e.printStackTrace();
 		}
 		return empresas;
+		/*SessionFactory sessionFactory = new Configuration().configure()
+				.buildSessionFactory();
+		Session session = sessionFactory.openSession();*/
 	}
 }
