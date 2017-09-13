52ff176bca2036fdd4907f629ae409cb8ae180cb
lautiallboys <lautiallboys@gmail.com>
2017-09-13 17:27:28 -0300

    persistencia metodologias

--- src/main/java/viewmodel/IngresarMetodologiaViewModel.java
+++ src/main/java/viewmodel/IngresarMetodologiaViewModel.java
@@ -44,7 +44,7 @@ public class IngresarMetodologiaViewModel {
 		builderMetodologia.setUnIndicador(indicadorSeleccionado);
 		
 		agregarComparadores();
-		
+		//
 	}
 
 	public void agregarComparadores() {
