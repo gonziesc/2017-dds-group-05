090f873544ef5de14419fce22b6acacfac16cf75
lautiallboys <lautiallboys@gmail.com>
2017-10-16 18:26:24 -0300

    login con modificaciones

---
+++ src/main/java/Services/UsuariosService.java
@@ -0,0 +1,76 @@
+package Services;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import model.Indicador;
+import model.Usuario;
+
+
+
+public class UsuariosService {
+	
+	public static List<Usuario> obtenerUsuariosDeServicioExterno() {
+	 SessionFactory sessionFactory = new Configuration().configure()
+			 .buildSessionFactory();
+	 Session session = sessionFactory.openSession();
+	 
+	 try {
+			
+			session.beginTransaction();
+			List<Usuario> results = (List<Usuario>) session.createQuery("FROM model.Usuario").getResultList();
+			session.getTransaction().commit();
+			return results;
+			
+		}
+		
+		catch (HibernateException e) {
+			if (session.getTransaction() != null) {
+	            session.getTransaction().rollback();
+	        }
+			
+		}
+		finally{
+			session.close();
+		}
+		return null;
+
+	}
+	/*
+	public static int obtenerUsuarioDeServicioExterno(String usu, String contra) {
+		 SessionFactory sessionFactory = new Configuration().configure()
+				 .buildSessionFactory();
+		 Session session = sessionFactory.openSession();
+		 
+		 try {
+				
+				session.beginTransaction();
+				Usuario results = (Usuario) session.createQuery("FROM model.Usuario where usuario= :usu AND contrasena= :contra ");
+				((Query) session).setParameter("usu", usu);
+				((Query) session).setParameter("contra",contra );
+
+				session.getTransaction().commit();
+				return 1;
+				
+			}
+			
+			catch (HibernateException e) {
+				if (session.getTransaction() != null) {
+		            session.getTransaction().rollback();
+		        }
+				return 0;
+				
+			}
+			finally{
+				session.close();
+			}
+		//	return 0;
+
+		}
+		*/
+}
--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,20 +1,49 @@
 package controllers;
 
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import Services.EmpresasService;
+import Services.UsuariosService;
+import model.Usuario;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
 public class HomeController {
-	public static ModelAndView home(Request req, Response res){
+	public ModelAndView home(Request req, Response res)throws FileNotFoundException{
 		return new ModelAndView(null, "home/home.hbs");
 	}
 	
-	public static ModelAndView showLogin(Request req, Response res){
+	
+	public ModelAndView showLogin(Request req, Response res){
+		String usuario = req.queryParams("usuario");
+		String contrasena = req.queryParams("contrasena");
+		List<Usuario> usuarios = UsuariosService.obtenerUsuariosDeServicioExterno();
+		if(usuario == null || contrasena ==null){
+			return new ModelAndView(null, "login/login.hbs");	
+		}
+		else{
+			if((usuarios.stream().anyMatch(unUsuario -> (usuario.equals(unUsuario.getUsuario()) &&  contrasena.equals(unUsuario.getContrasena()))))){
+				return new ModelAndView(null, "login/user.hbs");
+			}
+			else
+			{
+				return new ModelAndView(null, "login/login.hbs");
+			}
+		}	
+			
+			
+		
+	}
+	
+	public ModelAndView login(Request req, Response res){
+		// NO ESTA HACIENDO NADA
+		String usuario = req.queryParams("usuario");
+		String contrasena = req.queryParams("contrasena");
 		return new ModelAndView(null, "home/home.hbs");
 	}
 	
-	public static ModelAndView login(Request req, Response res){
-		return new ModelAndView(null, "home/home.hbs");
-	}
+	
 	
 }
