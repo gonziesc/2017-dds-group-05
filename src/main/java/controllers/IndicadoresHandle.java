e17824681ea179469dd66ad358ad9de38a47efa2
Santi Nucera <santinucera4@gmail.com>
2017-10-18 14:41:42 -0300

    No se subieron vistas

---
+++ src/main/java/controllers/IndicadoresHandle.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import model.Cuenta;
+import model.Indicador;
+
+public class IndicadoresHandle {
+	Map<String, List<Cuenta>> cuentas= new HashMap<>();
+	Map<String, List<Indicador>> indicadores = new HashMap<>();
+	public Map<String, List<Cuenta>> getCuentas() {
+		return cuentas;
+	}
+	public void setCuentas(Map<String, List<Cuenta>> cuentas) {
+		this.cuentas = cuentas;
+	}
+	public Map<String, List<Indicador>> getIndicadores() {
+		return indicadores;
+	}
+	public void setIndicadores(Map<String, List<Indicador>> indicadores) {
+		this.indicadores = indicadores;
+	}
+}
---
+++ src/main/java/controllers/MetodologiasHandle.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import model.Comparador;
+import model.ComparadorFiltro;
+import model.ComparadorOrden;
+import model.Indicador;
+
+public class MetodologiasHandle {
+	Map<String, List<ComparadorOrden>> comparadorOrden= new HashMap<>();
+	Map<String, List<ComparadorFiltro>> comparadorFiltro= new HashMap<>();
+	Map<String, List<Indicador>> indicadores = new HashMap<>();
+	
+	public Map<String, List<Indicador>> getIndicadores() {
+		return indicadores;
+	}
+	public void setIndicadores(Map<String, List<Indicador>> indicadores) {
+		this.indicadores = indicadores;
+	}
+	public Map<String, List<ComparadorOrden>> getComparadorOrden() {
+		return comparadorOrden;
+	}
+	public void setComparadorOrden(Map<String, List<ComparadorOrden>> comparadorOrden) {
+		this.comparadorOrden = comparadorOrden;
+	}
+	public Map<String, List<ComparadorFiltro>> getComparadorFiltro() {
+		return comparadorFiltro;
+	}
+	public void setComparadorFiltro(Map<String, List<ComparadorFiltro>> comparadorFiltro) {
+		this.comparadorFiltro = comparadorFiltro;
+	}
+	
+}
